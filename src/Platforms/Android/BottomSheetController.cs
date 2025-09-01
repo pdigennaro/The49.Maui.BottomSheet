53d9ace1e36ef570e49ad10401653db7febeeab4
Pietro Di Gennaro <pietrodig@live.com>
2025-09-01 14:00:21 +0200

    Apply strange Android fix

--- src/Platforms/Android/BottomSheetController.cs
+++ src/Platforms/Android/BottomSheetController.cs
@@ -532,7 +532,9 @@ public class BottomSheetController
         {
             layoutParams.Height += TopInset;
         }
-        _sheet.Arrange(new Rect(0, 0, _frame.Width / density, height));
+
+        // PD: this seems to fix dynamic layout issues in Android...
+        //_sheet.Arrange(new Rect(0, 0, _frame.Width / density, height));
     }
 
     public void Show(bool animated)
